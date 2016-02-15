8b6568958a9002eb0cbcfc063b8a6a03ac66f2ae
Alex-Gurung <aag1234@gmail.com>
2016-02-15 17:52:05 -0500

    Added ability to take in address and output hourly temperatures and general forcast

--- Main.py
+++ Main.py
@@ -1,4 +1,6 @@
 import speech_recognition as sr
+from geopy.geocoders import Nominatim
+import forecastio
 #Setup speech
 r = sr.Recognizer()
 
@@ -13,7 +15,7 @@ r = sr.Recognizer()
 #    print("Could not request results from Google Speech Recognition service; {0}".format(e))
 
 #This is code necessary for it to start listening for input
-with sr.Microphone() as source:
+'''with sr.Microphone() as source:
 	print("What is your name?")
 	audio = r.listen(source)
 
@@ -24,6 +26,35 @@ try:
 		print("Hello, Jarvis. Time for you to die.")
 	else:
 		print("Hello, %s, my name is Ultron." % name)
+except sr.UnknownValueError:
+    print("Google Speech Recognition could not understand audio")
+except sr.RequestError as e:
+    print("Could not request results from Google Speech Recognition service; {0}".format(e))'''
+
+#This is code necessary for it to start listening for input
+with sr.Microphone() as source:
+	print("Where do you live(Weather)?:")
+	audio = r.listen(source)
+
+geolocater = Nominatim()
+#geolocater = GeocoderDotUS(format_string="%s, Herndon VA")
+#Below are the try and except blocks for google speech recognition
+try:
+	place = r.recognize_google(audio)
+	print(place)
+	location = geolocater.geocode(place)
+	lat = location.latitude
+	print ("Latidude: %s" % lat)
+	long = location.longitude
+	print ("Longitude: %s" % long)
+	api_key = "161ff436ccbe7edb13357271e3462ae6" #Free from http://developer.forecast.io
+	forecast = forecastio.load_forecast(api_key, lat, long)
+	byHour = forecast.hourly()
+	print (byHour.summary.encode('utf8'))
+	print "Icon" + byHour.icon
+	print "Temperatures: "
+	for hourlyData in byHour.data:
+		print hourlyData.temperature
 except sr.UnknownValueError:
     print("Google Speech Recognition could not understand audio")
 except sr.RequestError as e:
