70a8674f8b6b5246b35ecad5461300ec886b6d69
Alex-Gurung <aag1234@gmail.com>
2016-02-15 00:23:19 -0500

    Just added some comments to make it more readable next time I look at it

--- Main.py
+++ Main.py
@@ -1,6 +1,7 @@
 import speech_recognition as sr
-
+#Setup speech
 r = sr.Recognizer()
+
 #with sr.Microphone() as source:
 #	print("Say something!")
 #	audio = r.listen(source)
@@ -11,11 +12,12 @@ r = sr.Recognizer()
 #except sr.RequestError as e:
 #    print("Could not request results from Google Speech Recognition service; {0}".format(e))
 
-
+#This is code necessary for it to start listening for input
 with sr.Microphone() as source:
 	print("What is your name?")
 	audio = r.listen(source)
 
+#Below are the try and except blocks for google speech recognition
 try:
 	name = r.recognize_google(audio)
 	if(name.lower() == "jarvis"):
